b96ba1a95ef2c1be1574f2fb85691464921d27b5
rzalwhy12 <rzalcorp05@gmail.com>
2025-07-04 05:01:22 +0700

    scroll kebawah

--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 
 import ArrowDownIcon from '../assets/images/Scroll.svg';
